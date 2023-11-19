f263c660e80968cfe820c779e01af24e718e2055
Dharmender <dharmenderarya477@gmail.com>
2023-11-19 10:36:20 +0530

    E-Commerce Webste using HTML CSSS and JavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+// Script for navigation bar
+// Catch the Element 
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const cross = document.getElementById('cross');
+
+
+if(bar) {
+    bar.addEventListener('click', ()=> {
+        nav.classList.add('active')
+    })
+}
+if(cross) {
+    cross.addEventListener('click', ()=> {
+        nav.classList.remove('active')
+    })
+}
